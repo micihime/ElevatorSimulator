012c24910e9374ad45a1667839af534b82ccdcb4
Miroslava Hreskova <hroncovam@yahoo.com>
2025-01-07 17:23:02 +0100

    Creating Elevator class with current floor property

---
+++ ElevatorSimulator/Elevator.cs
@@ -0,0 +1,16 @@
+﻿namespace ElevatorSimulator
+{
+    public class Elevator
+    {
+        private int currentFloor;
+
+        public int CurrentFloor { get => currentFloor; }
+
+        public Elevator() 
+        {
+            this.currentFloor = 1;
+        }
+
+
+    }
+}
