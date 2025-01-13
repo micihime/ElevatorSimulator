23a8e93c7821d3a1679fc75a494773c88383f33d
Miroslava Hreskova <hroncovam@yahoo.com>
2025-01-13 14:17:14 +0100

    Creating folder to practice class design - will contain more projects

---
+++ 01 class design/01 elevator/ElevatorSimulator/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Welcome to Elevator Simulator!");
+Console.WriteLine("");
+
+ElevatorSimulator.Elevator elevator = new ElevatorSimulator.Elevator();
+elevator.Call(5);
+elevator.GetIn();
+elevator.TravelWith(7);
+elevator.GetOut();
+
+Console.WriteLine("");
+Console.WriteLine("Simulation finished!");
+Console.ReadLine();
