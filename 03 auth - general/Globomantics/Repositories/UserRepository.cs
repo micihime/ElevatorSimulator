401ac5f21dd99c19300c04aa403664631cc88b40
Miroslava Hreskova <hroncovam@yahoo.com>
2025-02-13 14:50:27 +0100

    User test with password password

--- 03 auth - general/Globomantics/Repositories/UserRepository.cs
+++ 03 auth - general/Globomantics/Repositories/UserRepository.cs
@@ -6,9 +6,9 @@ public class UserRepository : IUserRepository
 {
     private List<UserModel> users = new () 
     {
-        new UserModel { Id = 3522, Name = "roland", Password = "K7gNU3sdo+OL0wNhqoVWhr3g6s1xYv72ol/pe/Unols=",
-            FavoriteColor = "blue", Role = "Admin", GoogleId = "101517359495305583936" }
-        //new UserModel { Id = 123, Name = "test", Password = "", Role = "Admin", FavoriteColor = "green" }
+        //new UserModel { Id = 3522, Name = "roland", Password = "K7gNU3sdo+OL0wNhqoVWhr3g6s1xYv72ol/pe/Unols=",
+        //    FavoriteColor = "blue", Role = "Admin", GoogleId = "101517359495305583936" }
+        new UserModel { Id = 123, Name = "test", Password = "XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg=", Role = "Admin", FavoriteColor = "green" }
     };
 
     public UserModel? GetByUsernameAndPassword(string username, string password)
