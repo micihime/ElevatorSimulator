f1bb3b1c1a2d8ddc4e425672b1169b134b78e8b8
Miroslava Hreskova <hroncovam@yahoo.com>
2025-02-13 12:31:21 +0100

    Bugfix - search by username and password

--- 03 auth - general/Globomantics/Repositories/UserRepository.cs
+++ 03 auth - general/Globomantics/Repositories/UserRepository.cs
@@ -6,13 +6,15 @@ public class UserRepository : IUserRepository
 {
     private List<UserModel> users = new () 
     {
-        new UserModel { Id = 123, Name = "test", Password = "", Role = "Admin", FavoriteColor = "green" }
+        new UserModel { Id = 3522, Name = "roland", Password = "K7gNU3sdo+OL0wNhqoVWhr3g6s1xYv72ol/pe/Unols=",
+            FavoriteColor = "blue", Role = "Admin", GoogleId = "101517359495305583936" }
+        //new UserModel { Id = 123, Name = "test", Password = "", Role = "Admin", FavoriteColor = "green" }
     };
 
     public UserModel? GetByUsernameAndPassword(string username, string password)
     {
         var user = users.SingleOrDefault(
-            u => u.Name && u.Password == password.Sha256());
+            u => u.Name == username && u.Password == password.Sha256());
         return user;
     }
 }
