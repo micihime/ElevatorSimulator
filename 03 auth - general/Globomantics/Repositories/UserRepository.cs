49c857044983b168eb732178f87ba6554c21f026
Miroslava Hreskova <hroncovam@yahoo.com>
2025-02-13 12:27:30 +0100

    Adding UserRepository and interface for it - for simplification, the user data is stored in memory

---
+++ 03 auth - general/Globomantics/Repositories/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using Globomantics.Models;
+
+namespace Globomantics.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private List<UserModel> users = new () 
+    {
+        new UserModel { Id = 123, Name = "test", Password = "", Role = "Admin", FavoriteColor = "green" }
+    };
+
+    public UserModel? GetByUsernameAndPassword(string username, string password)
+    {
+        var user = users.SingleOrDefault(
+            u => u.Name && u.Password == password.Sha256());
+        return user;
+    }
+}
